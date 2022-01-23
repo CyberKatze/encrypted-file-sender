d61e1851ebae36a44debda8cff40ed89c8b586c9
Mehradad Shahidi <m.mehrdadshahidi@gmail.com>
2022-01-23 09:57:55 +0330

    Add Commnad-line interface for handling commands
    - Add subcommand `connect [-p <PORT>] <IP>` for clinet-side
    - Add subcommand `listen [-p <PORT>]` for server-side
    - Add features in README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# Installation
+
+
+# Features 
+- [x] CLI interface with usage
+- [ ] Sending data over TCP connection
+- [ ] Sending file over TCP connection
+- [ ] Encrypt file with AES or DES and share key
+- [ ] Show encryption time and transmission time   
+- [ ] Generate some random files with different size
+- [ ] Make CSV file for timing 
+- [ ] Openssl
+- [ ] Plot the CSV file (optional)
+- [ ] Windows compatibility (optional) 
+- [ ] Unit testing for app (optional)
+- [ ] Generate wiki with Godoc (optional)
+
+# Usage
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,22 +1,99 @@
-package cmd
+// Package cmd is command-line interface for encrypted file sender.
+package main
 
 import (
-	"flag"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
+// Variables that is being set by CLI
 var (
-	IsListen bool
-	Port     int
-	IP	string	
+  IsListen  bool
+  Port      string
+  IP        string
 )
 
-func init() {
-	flag.BoolVar(&IsListen, "l", false, "Bind and listen for incomming connection")
-	flag.IntVar(&Port, "p", 8888, "Set the port for TCP connection. The Default port is 8888")
-	flag.Parse()
-	IP =flag.Arg(0)
+// connectAction do actions required for client-side
+func connectAction(c *cli.Context) error{
+  if c.Args().Len() >0 {
+  IsListen = false
+  IP = c.Args().Get(0)
+  Port = c.String("port")
+  fmt.Printf("connect to %s:%s",IP, Port)
+  return nil
+  }
+
+  return fmt.Errorf("no <IP> is specified, set the <IP> " +
+  "that you want to connect to")
 }
 
-func Run() error{
-	return nil	
+// listenAction do actions requred for server-side
+func listenAction(c *cli.Context) error{
+  IsListen = true
+  Port = c.String("port")
+  fmt.Printf("listen to 0.0.0.0:%s", Port)
+  return nil
+  
+}
+
+func main() {
+  // Version of app
+  cli.VersionFlag = &cli.BoolFlag{
+    Name:  "version",
+    Aliases: []string{"v"},
+    Usage: "output version information",
+  }
+
+// CLI app spec
+  app := &cli.App{
+    Name:     "encrypted-file-sender",
+    Usage:    "send encrypted file over network",
+    Version:  "1.0.0",
+  }
+
+// Global flags
+  app.Flags = []cli.Flag{
+    &cli.BoolFlag{
+      Name:     "verbose",
+      Aliases:  []string{"V"}, 
+      Usage: "verbose output",
+    },
+  }
+
+// share flag between multiple subcommand
+  portFlag := &cli.StringFlag{
+          Name:     "port",
+          Aliases:  []string{"p"}, 
+          Usage:    "Define `PORT`",
+          Required: true,
+        }
+
+// CLI subcommands
+  app.Commands = []*cli.Command{
+    {
+      Name:     "connect",
+      Category: "Network",
+      Usage:    "Connect [-p <PORT>] <IP>",
+      Flags:    []cli.Flag{
+        portFlag,
+      },
+      Action:   connectAction,
+    },
+    {
+      Name:     "listen",
+      Category: "Network",
+      Usage:    "listen [-p <PORT>]",
+      Flags:    []cli.Flag{
+        portFlag,
+      },
+      Action:   listenAction,
+    },
+  }
+
+  // RUn app
+  if err:= app.Run(os.Args); err != nil {
+    fmt.Println(err)
+  }
 }
